8393f23156799eadd3b491299b9ae1e72b90577b
rhpamAdmin <>
2020-01-09 19:42:36 +0400

    {/src/main/java/com/security/security_compliance/ComplianceService.java}

--- src/main/java/com/security/security_compliance/ComplianceService.java
+++ src/main/java/com/security/security_compliance/ComplianceService.java
@@ -15,6 +15,13 @@ public class ComplianceService implements java.io.Serializable {
 	@javax.persistence.SequenceGenerator(sequenceName = "COMPLIANCESERVICE_ID_SEQ", name = "COMPLIANCESERVICE_ID_GENERATOR")
 	private java.lang.Long id;
 
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
+
+	@javax.persistence.OneToMany(fetch = javax.persistence.FetchType.EAGER, cascade = {javax.persistence.CascadeType.ALL})
+	@org.kie.api.definition.type.Label(value = "Check List")
+	private java.util.List<com.security.security_compliance.ComplianceItem> checkList;
+
 	public ComplianceService() {
 	}
 
@@ -26,8 +33,30 @@ public class ComplianceService implements java.io.Serializable {
 		this.id = id;
 	}
 
-	public ComplianceService(java.lang.Long id) {
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.util.List<com.security.security_compliance.ComplianceItem> getCheckList() {
+		return this.checkList;
+	}
+
+	public void setCheckList(
+			java.util.List<com.security.security_compliance.ComplianceItem> checkList) {
+		this.checkList = checkList;
+	}
+
+	public ComplianceService(
+			java.lang.Long id,
+			java.lang.String name,
+			java.util.List<com.security.security_compliance.ComplianceItem> checkList) {
 		this.id = id;
+		this.name = name;
+		this.checkList = checkList;
 	}
 
 }
